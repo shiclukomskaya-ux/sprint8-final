c9c52ae1608edad4a88edff6487f32874eb3bd8e
Nastya <shic.lukomskaya@gmail.com>
2026-06-16 23:21:13 +0300

    Добавлены функции Add и Get

--- parcel.go
+++ parcel.go
@@ -16,7 +16,20 @@ func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
 
 	// верните идентификатор последней добавленной записи
-	return 0, nil
+
+	res, err := s.db.Exec("INSERT INTO parcel (client, status, address, created_at) VALUES (:client, :status, :address, :created_at)",
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
